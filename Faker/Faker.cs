abb09742887118bbf9f55430681b51d18c5b556d
vlazhv <danilabondarev1234@gmail.com>
2022-10-30 21:11:04 +0300

    initial commit. Common structure is ready

---
+++ Faker/Faker.cs
@@ -0,0 +1,17 @@
+﻿using Faker.Core;
+
+namespace Faker
+{
+	public class Faker : IFaker
+	{
+		public T Create<T>()
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Create( Type t )
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Faker/IFaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Core
+{
+	public interface IFaker
+	{
+		public T Create<T>();
+
+		public object Create( Type t );
+	}
+}
